0b07b3e13229e8c945fbc65fe2d835440ca2c9d6
madol <dingkuoldit@gmail>
2024-07-25 22:58:20 +0300

    changes made on the project

--- Ecoweb/Ecoweb/settings.py
+++ Ecoweb/Ecoweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y=p@v*wqwk_fk)87u4tgmyzgmf4j6&+y#3!hjw(ctwd32=9xc6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition

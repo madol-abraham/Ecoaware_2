1acd14595a96110bb52dc157ed0af59c6acaac0d
madol <dingkuoldit@gmail>
2024-08-02 02:13:43 +0300

    updates

--- Ecoweb/settings.py
+++ Ecoweb/settings.py
@@ -65,8 +65,9 @@ ROOT_URLCONF = 'Ecoweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [BASE_DIR / 'templates'],
-        'DIRS': ['templates'],
+#       'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+#        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
